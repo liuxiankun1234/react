904bafe09381a5b7a3ac317714a91de839f471b5
liuxiankun <liuxiankun@zhangyue.com>
2021-04-19 09:59:04 +0800

    feat:learn react

--- question.js
+++ question.js
@@ -61,4 +61,120 @@
  * 
  *  的深度解析使用索引作为 key 的负面影响
  *      https://robinpokorny.medium.com/index-as-a-key-is-an-anti-pattern-e0349aece318
+ * 
+ * 
+ * 
+ *  生命周期相关
+ *      生命周期图谱
+ *          https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+ *      具体的生命周期
+ *          挂载
+ *              constructor()
+ *              render()
+ *              componentDidMount()
+ *              static getDerivedStateFromProps(); 不常用
+ *          更新
+ *              render()
+ *              componentDidUpdate()
+ *              static getDerivedStateFromProps() 不常用
+ *              shouldComponentUpdata() 不常用
+ *              getSnapshotBeforeUpdate() 不常用
+ *          卸载 当组件从DOM中移除时调用
+ *              componentWillUnmount()  
+ *          错误处理 当渲染过程、生命周期、或子组件的构造函数中抛错调用
+ *              static getDerivedStateFromError()
+ *              componentDidCatch()
+ *          其他API
+ *              setState()
+ *              forceUpdate()
+ *          Class属性
+ *              defaultProps()
+ *              defaultName
+ *          实例属性
+ *              props
+ *              state
+ *      常用生命周期
+ *          render()
+ *              render方法是class组件唯一必须实现的方法
+ *              当render被调用时，它会检查this.props和this.state的变化并返回以下类型
+ *                  React元素
+ *                      通常通过JSX创建 <div/> 会被渲染为DOM节点 <MyComponent>会被渲染成自定义组件
+ *                      无论是div还是MyComponent都是React元素
+ *                  数组或则fragment
+ *                      fragment 
+ *                          相当于一个空标签 类似于 document.createDocumentFragment() 创建一个空的文档碎片
+ *                          应用场景
+ *                              table 的 td 被做成一个组件  
+ *                              如果td组件内部被 div包裹 则不能正确渲染table
+ *                              可以使用 <React.fragment><td>11</td></React.fragment>
+ *                  Portals
+ *                      https://zh-hans.reactjs.org/docs/portals.html
+ *                  字符串或数值类型
+ *                      DOM中渲染成文本节点
+ *                  布尔类型或null
+ *                      什么都不渲染 主要用于 canRenderComponent && <Child />
+ *              注意
+ *                  shouldComponentUpdate返回false，则不会调用render()
+ *          constructor()
+ *              如果不初始化state或不进行方法绑定，则不需要为React组件实现构造函数
+ *              React挂载之前会调用它的构造函数。在为React.Component子类实现构造函数时，应在其它语句之前调用super(props)。
+ *              否则this.props在构造函数中可能回出现未定义的bug
+ *              
+ *              通常React中，构造函数仅用于处理一下两种情况
+ *                  给this.state赋值对象来初始化内部state
+ *                  为事件处理函数绑定实例
+ *              在constructor函数中不要调用setState()方法。如果你的组件需要使用内部state，请直接在构造函数中为this.state赋值初始state
+ *              
+ *              
+ *              注意
+ *                  ？？ 要避免在构造函数中引入任何副作用或订阅。如果遇到此场景，请将对应的操作放置在componentDidMount中
+ *                  避免将props的值复制给state！这是一个常见的错误
+ *                      this.state = {color: props.color}
+ *                      可以直接使用props.color，同时还产生了bug(更新props中的color，并不影响state)
+ *                  只有在你刻意忽略prop更新的情况下使用。此时应将prop重命名为initialColor或defaultColor。必要时可以修改key 已强制重置其内部state
+ *                  constructor中state依赖props处理方式
+ *                      https://zh-hans.reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
+ *          componentDidMount()
+ *              componentDidMount()会在组件挂载后（插入DOM树中）立即调用，依赖于DOM节点的初始化应该放在这里。
+ *              可以在componetDidMount()里直接调用setState()。它将会触发额外的渲染，但此渲染会发生在浏览器更新屏幕之前。可以保证即使在render两次调用的情况下，用户也看不到中间状态。
+ *              此生命周期慎用setState() 性能问题
+ *              适合做哪些事
+ *                  网络请求或者数据    
+ *                  添加订阅 如果添加了订阅记得在componentWillUnmount()里取消订阅
+ *                  渲染依赖于DOM节点的大小和位置，实现modals、tooltips等情况
+ *          componentDidUpdate()
+ *              componentDidUpdate()会在更新后立即调用
+ *              在非语句中调用setState()会导致死循环
+ *              适合做哪些事
+ *                  如果对更新前后的props进行了比较，也可以选择在此处进行网络请求。
+ *              注意
+ *                  shouldComponentUpdate()返回false，则不会调用componentDidUpdate()
+ *          componentWillUnmount()
+ *              componentWillUnmount()会在组件卸载及销毁之前直接调用。在此方法中执行必要的清理操作
+ *                  适合做哪些事
+ *                      清楚timer
+ *                      取消网络请求
+ *                      清楚componentDidMount()中创建的订阅
+ *      不常用的生命周期
+ *          shouldComponentUpdate()
+ *              根据shouldComponentUpdate()的返回值，判断React组件的输出是否受到当前state或props更改的影响。默认state每次发生变化组件都重新渲染。大部分情况应该遵循默认情况
+ *              当props或state发生变化，shouldComponentUpdate()应该在渲染执行之前被调用。返回默认值true。
+ *              此方法仅作为心更难优化的方式存在。不要企图依靠此方法来阻止渲染，有可能产生bug
+ *              应该烤炉使用内置的PureCompoent组件，而不是手动编写shouldComponentUpdate()。
+ *                  PureCompoent组件会对props和state进行浅层比较，并减少了跳过必要更新的可能
+ *              注意
+ *                  首次渲染或者forceUpdate时不会调用该方法        
+ *                  如果一定要手动编写此函数，可以将this.props和nextProps以及this.state与nextState进行比较，并返回false以告知React可以跳过更新
+ *                  不建议在此生命周期中进行深层比较或者使用JSON.stringify()。影响效率，且会损害性能
+ *                  目前如果返回false，则不会调用UNSAFE_componentWillUpdate()，render() 和 componentDidUpdate()。后续版本，React可能会将此返回视为提示而不是严格的指令，并且返回false，仍可能重新渲染
+ *          staic getDerivedStateFromProps()
+ *              改方法会在调用render方法之前调用，并且在初始挂载及后续更新时被调用
+ *              未完待续。。。。。。。。。。。。。。。。。。。
+ *                      
+ * 
+ *  待观看的内容
+ *      https://zh-hans.reactjs.org/docs/portals.html
+ *      https://zh-hans.reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
+ *      PureCompoent组件
+ *              
 */
